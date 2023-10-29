6e358e8a5f118f6adb87632343f02852938d5392
Philipp Amster <139384133+accura7e@users.noreply.github.com>
2023-10-29 13:19:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const tabBtns = document.querySelectorAll('[data-tab]');
+const cardBoxes = document.querySelectorAll('[data-tab-content]')
+
+tabBtns.forEach(function(item){
+
+    item.addEventListener('click', function(){
+        
+        cardBoxes.forEach(function(item){
+                item.classList.add('none');
+        });
+
+        tabBtns.forEach(function(item){
+            item.classList.remove('tabs-btn--active')
+        });
+        const cardBtn = document.querySelector(`[data-tab='${this.dataset.tab}']`);
+        const cardBox = document.querySelector(`#${this.dataset.tab}`);
+
+        cardBox.classList.remove('none')
+        cardBtn.classList.add('tabs-btn--active')
+
+    })
+    
+})
+
+
+const formFields = document.querySelectorAll('.form__item-field');
+
+formFields.forEach(function(item){
+    const thisParent = item.closest('.form__item');
+    const thisPlaceholder = thisParent.querySelector('.fake-placeholder');
+
+    item.addEventListener('focus', function(){
+        thisPlaceholder.classList.add('active')
+    });
+
+    item.addEventListener('blur', function(){
+        if(item.value.length > 0){
+            thisPlaceholder.classList.add('active')
+        } else{
+            thisPlaceholder.classList.remove('active')
+        }
+    });
+
+  
+})
