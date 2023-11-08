3418bb5a5307609f51e0515e6ac85fa96e8c154f
accura7e <accura7ee@gmail.com>
2023-11-08 19:08:16 +0300

    add: added php mailer

--- js/main.js
+++ js/main.js
@@ -630,3 +630,60 @@ cards.forEach(function(item){
     })
 
 
+    $(document).ready(function () {
+
+
+        //FORM VALIDATE
+        $('.form').validate({
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                },
+                
+                message: {
+                    required: true
+                }
+            },
+            messages: {
+                email: {
+                    required: 'Введите email',
+                    email: 'отсутсвует символ @'
+                },
+                
+                message: {
+                    required: 'Поле не должно быть пустым'
+                }
+            },
+            submitHandler: function (form) {
+                ajaxFormSubmit();
+            }
+    
+        });
+    
+        //*************************************************** */
+        // Функция AJAX запрса на сервер
+    
+        function ajaxFormSubmit() {
+    
+            let string = $(".form").serialize(); // Соханяем данные введенные в форму в строку.
+    
+            //Формируем ajax запрос
+            $.ajax({
+                type: "POST", // Тип запроса - POST
+                url: "php/mail.php", // Куда отправляем запрос
+                data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+    
+                // Функция если все прошло успешно
+                success: function (html) {
+                    $(".form").slideUp(800);
+                    $('#answer').html(html);
+                }
+            });
+            // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+            return false;
+        }
+    
+    
+    
+    });
